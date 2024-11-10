23dc21d8e6e6260b64afb82a90e6630482508e66
carloshobmeier <carloshobmeier@hotmail.com>
2024-11-10 16:19:37 -0300

    questão 1 e estrutura inicial

---
+++ Somativo2_DBPT.mongodb.js
@@ -0,0 +1,113 @@
+// EQUIPE:
+// Amanda Queiroz Sobral
+// Carlos Eduardo Domingues Hobmeier
+// João Vitor de Freitas
+
+
+// 1. Modelagem de Dados:
+//    - Desenhe um esquema de modelagem de dados para o sistema no MongoDB.
+//      Considere o uso de documentos incorporados e referências onde apropriado.
+//      Pode ser feito em UML ou em DER.
+
+
+
+
+// 2. Inserção e Validação:
+
+//    - Crie collections e schema validations necessários.
+
+//    - Insira alguns usuários, produtos e categorias de exemplo no banco de dados
+//      (pelo menos 5 para cada).
+
+
+
+
+
+// 3. Consultas:
+
+//    - Escreva uma consulta para encontrar todos os produtos em uma categoria específica.
+
+//    - Escreva uma consulta para encontrar todas as avaliações de um produto específico.
+
+//    - Escreva uma consulta para criar uma nova transação.
+
+//    - Escreva uma consulta para atualizar a quantidade disponível de um produto após uma compra.
+
+
+
+
+
+// 4. Índices:
+
+//    - Pense sobre quais índices seriam úteis para otimizar as consultas mais comuns.
+
+
+
+
+
+// 5. Agregações:
+
+//    - Escreva uma consulta de agregação para encontrar a média de avaliações para cada produto.
+
+//    - Escreva uma consulta de agregação para encontrar o total de vendas para cada categoria.
+
+
+
+
+
+
+// Depois da sprint inicial, os stackholders decidiram algumas mudanças em relação ao projeto inicial.
+// Você deve realizar todas as mudanças sem que tenha perda de dados ou perca de integridade dos dados.
+
+
+// 6. Promoções:
+
+//    - Implemente uma funcionalidade que permita aos vendedores oferecerem descontos em seus produtos
+//      por um período limitado.
+
+
+
+
+
+// 7. Pontos de Fidelidade:
+
+//    - Implemente um sistema de pontos de fidelidade onde os usuários ganham pontos por cada compra,
+//      que podem ser usados para descontos em futuras compras.
+
+
+
+
+
+// 8. Resposta a Avaliações:
+
+//    - Permita que os vendedores respondam às avaliações deixadas em seus produtos.
+
+
+
+
+
+// 9. Geolocalização
+
+//   - Os usuários podem definir sua localização geográfica.
+
+//   - Os produtos têm a localização geográfica do vendedor associada a eles.
+
+//   - crie indices caso ache necessário.
+
+//   - Os usuários podem buscar produtos com base na proximidade geográfica, podendo filtrar os
+//      resultados por raio de distância.
+
+//   - Escreva uma consulta de agregação para encontrar a média de distância entre compradores
+//      e vendedores para transações concluídas.
+
+//   - Escreva uma consulta de agregação para encontrar a categoria de produto mais popular em
+//      uma área geográfica específica.
+
+
+
+
+
+
+// 10. Relatórios:
+
+//     - Crie consultas de agregação para gerar relatórios de vendas para os vendedores.
